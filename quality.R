ec3515b3c85ebf445ba778bfefbf10fb4e752c46
paul <paul.bouchequet@gmail.com>
2016-02-11 13:06:22 +0100

    Qualité des données

---
+++ quality.R
@@ -0,0 +1,12 @@
+# Visualiser les erreurs
+library(ggplot2)
+        
+df <- read.csv("data/Cantine-Mirabeau.csv")
+df$datetime <- strptime(paste(df$Date,df$Heure) ,format = "%d/%m/%y %H:%M:%S")
+df$Caisse <- as.factor(df$Caisse)
+ggplot( df , aes(x=datetime, y=Ticket)) +
+  geom_line(aes(group=Caisse, color=Caisse))
+
+
+# Ticket minimum Caisse 4
+min(df$Ticket[df$Caisse == 4])
